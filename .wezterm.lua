6a4d2bf5837b17d5ed3703b677501beae51d3363
zsb258 <zhuang.shengbin@u.nus.edu>
2025-11-02 23:46:18 +0800

    update wezterm; add ocaml opam

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,30 +12,33 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
+-- fix error when having both x11 & wayland
+config.enable_wayland = false
 
 -- For example, changing the color scheme:
 config.color_scheme = "OneHalfDark"
 
-config.font = wezterm.font{ family="FiraCodeNerdFontMono", weight="Bold" }
+config.font = wezterm.font({ family = "FiraCodeNerdFontMono", weight = "Bold" })
 config.font_size = 14.0
 
-config.keys = {
-  {
-    key = 'E',
-    mods = 'CTRL|SHIFT',
-    action = act.PromptInputLine {
-      description = 'Enter new name for tab',
-      action = wezterm.action_callback(function(window, pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    },
-  },
+warn_about_missing_glyphs = false
 
+config.keys = {
+	{
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 for i = 1, 8 do
 	-- CTRL+ALT + number to activate that tab
--- .zshrc
+++ .zshrc
@@ -61,3 +61,16 @@ fi
 unset __conda_setup
 # <<< conda initialize <<<
 
+# vi motions
+set -o vi
+# vi motions end
+#
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+[[ ! -r '/home/ziyi/.opam/opam-init/init.zsh' ]] || source '/home/ziyi/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+# END opam configuration
